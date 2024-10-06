7378c18aeef3fb0bfdaf3b1f1fad9d4ab3c4cb1d
alantothe <rubenamalpartida@gmail.com>
2024-10-05 22:08:38 -0400

    clean up

--- src/types/dto/CreateUser.dto.ts
+++ src/types/dto/CreateUser.dto.ts
@@ -1,16 +1,5 @@
-export type CreateUserDto ={
+export type CreateUserDto = {
     username: string;
-    email:string;
-    password:string;
+    email: string;
+    password: string;
 }
-
-function idValue<Type>(arg:Type): Type{
-    return arg
-}
-
-const number = idValue(7)
-
-const string =idValue('alan')
-
-console.log(number)
-console.log(string)
