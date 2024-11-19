8fe905d52a77ea6be67ded416db78daf598d0802
alantothe <rubenamalpartida@gmail.com>
2024-11-19 17:44:48 -0500

    install cors

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,19 @@
-import db from "./mongo/connection"
-import express from "express"
-import routes from "./routes/index"
-import { configureGoogleAuth, setupGoogleStrategy } from './plugin/google';
+import db from "./mongo/connection";
+import express from "express";
+import routes from "./routes/index";
+import { configureGoogleAuth, setupGoogleStrategy } from "./plugin/google";
+import cors from "cors";
 
-const app = express()
-const PORT = 4000
+const app = express();
+const PORT = 4000;
+
+app.use(
+  cors({
+    origin: "http://localhost:3000", // your frontend url
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.use(express.json());
 
@@ -12,14 +21,12 @@ app.use(express.json());
 setupGoogleStrategy();
 configureGoogleAuth(app);
 
-app.use("/api", routes)
-
+app.use("/api", routes);
 
 db.on("connected", () => {
-    console.clear();
-    console.log("Connected to MongoDB");
-    app.listen(PORT, () => {
-      console.log(`Express server running on PORT: ${PORT}`);
-    });
+  console.clear();
+  console.log("Connected to MongoDB");
+  app.listen(PORT, () => {
+    console.log(`Express server running on PORT: ${PORT}`);
   });
-  
+});
